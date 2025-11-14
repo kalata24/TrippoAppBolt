c3aff16af24f2fc4719f949e4d1d8e8e99713297
kalata24 <wowacctomi@gmail.com>
2025-11-14 13:46:47 +0200

    Updated app.config.js

--- app.config.js
+++ app.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     extra: {
       eas: {
-        projectId: "4211d03b-f0cb-41e2-8699-c4b93b1af8cc"
+        projectId: "d47540bf-3733-4ae2-a902-d787869ee8b0"
       },
       EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL || "https://haiiwytkyojxspaslqqa.supabase.co",
       EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhhaWl3eXRreW9qeHNwYXNscXFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzM4MDEsImV4cCI6MjA3ODUwOTgwMX0.0vcUTeWAzQD5Nd0OVKQvLRw35ILLsmZcy4kaxgMJGk0"
